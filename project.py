1d81cc2a3bdb3c870c46dda745f174a656736ce7
Gvym <massonry3d@gmail.com>
2024-05-29 17:20:39 +0300

    Create project.py

---
+++ project.py
@@ -0,0 +1,231 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+%matplotlib inline
+sns.set_style("whitegrid")
+plt.style.use("fivethirtyeight")
+
+# example text for model training (SMS messages)
+simple_train = ['call you tonight', 'Call me a cab', 'Please call me... PLEASE!']
+
+# import and instantiate CountVectorizer (with the default parameters)
+from sklearn.feature_extraction.text import CountVectorizer
+
+vect = CountVectorizer()
+
+# learn the 'vocabulary' of the training data (occurs in-place)
+vect.fit(simple_train)
+
+# examine the fitted vocabulary
+vect.get_feature_names_out()
+
+# transform training data into a 'document-term matrix'
+simple_train_dtm = vect.transform(simple_train)
+simple_train_dtm
+
+# convert sparse matrix to a dense matrix
+simple_train_dtm.toarray()
+
+# examine the vocabulary and document-term matrix together
+pd.DataFrame(simple_train_dtm.toarray(), columns=vect.get_feature_names())
+
+# check the type of the document-term matrix
+print(type(simple_train_dtm))
+
+# examine the sparse matrix contents
+print(simple_train_dtm)
+
+# example text for model testing
+simple_test = ["please don't call me"]
+
+# transform testing data into a document-term matrix (using existing vocabulary)
+simple_test_dtm = vect.transform(simple_test)
+simple_test_dtm.toarray()
+
+# examine the vocabulary and document-term matrix together
+pd.DataFrame(simple_test_dtm.toarray(), columns=vect.get_feature_names_out())
+
+sms = pd.read_csv("/kaggle/input/sms-spam-collection-dataset/spam.csv", encoding='latin-1')
+sms.dropna(how="any", inplace=True, axis=1)
+sms.columns = ['label', 'message']
+
+sms['label_num'] = sms.label.map({'ham':0, 'spam':1})
+sms.head()
+
+sms['message_len'] = sms.message.apply(len)
+sms.head()
+
+plt.figure(figsize=(12, 8))
+
+sms[sms.label=='ham'].message_len.plot(bins=35, kind='hist', color='blue', 
+                                       label='Ham messages', alpha=0.6)
+sms[sms.label=='spam'].message_len.plot(kind='hist', color='red', 
+                                       label='Spam messages', alpha=0.6)
+plt.legend()
+plt.xlabel("Message Length")
+
+import string
+from nltk.corpus import stopwords
+
+def text_process(mess):
+    """
+    Takes in a string of text, then performs the following:
+    1. Remove all punctuation
+    2. Remove all stopwords
+    3. Returns a list of the cleaned text
+    """
+    STOPWORDS = stopwords.words('english') + ['u', 'ü', 'ur', '4', '2', 'im', 'dont', 'doin', 'ure']
+    # Check characters to see if they are in punctuation
+    nopunc = [char for char in mess if char not in string.punctuation]
+
+    # Join the characters again to form the string.
+    nopunc = ''.join(nopunc)
+    
+    # Now just remove any stopwords
+    return ' '.join([word for word in nopunc.split() if word.lower() not in STOPWORDS])
+
+sms['clean_msg'] = sms.message.apply(text_process)
+type(stopwords.words('english'))
+
+from collections import Counter
+
+words = sms[sms.label=='ham'].clean_msg.apply(lambda x: [word.lower() for word in x.split()])
+ham_words = Counter()
+
+for msg in words:
+    ham_words.update(msg)
+    
+words = sms[sms.label=='spam'].clean_msg.apply(lambda x: [word.lower() for word in x.split()])
+spam_words = Counter()
+
+for msg in words:
+    spam_words.update(msg)
+
+# split X and y into training and testing sets 
+from sklearn.model_selection import train_test_split
+
+# how to define X and y (from the SMS data) for use with COUNTVECTORIZER
+X = sms.clean_msg
+y = sms.label_num
+print(X.shape)
+print(y.shape)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
+
+print(X_train.shape)
+print(X_test.shape)
+print(y_train.shape)
+print(y_test.shape)
+
+from sklearn.feature_extraction.text import CountVectorizer
+
+# instantiate the vectorizer
+vect = CountVectorizer()
+vect.fit(X_train)
+
+# learn training data vocabulary, then use it to create a document-term matrix
+X_train_dtm = vect.transform(X_train)
+
+# equivalently: combine fit and transform into a single step
+X_train_dtm = vect.fit_transform(X_train)
+
+# transform testing data (using fitted vocabulary) into a document-term matrix
+X_test_dtm = vect.transform(X_test)
+
+from sklearn.feature_extraction.text import TfidfTransformer
+
+tfidf_transformer = TfidfTransformer()
+tfidf_transformer.fit(X_train_dtm)
+tfidf_transformer.transform(X_train_dtm)
+
+# import and instantiate a Multinomial Naive Bayes model
+from sklearn.naive_bayes import MultinomialNB
+nb = MultinomialNB()
+
+%time nb.fit(X_train_dtm, y_train)
+
+from sklearn import metrics
+
+# make class predictions for X_test_dtm
+y_pred_class = nb.predict(X_test_dtm)
+
+# calculate accuracy of class predictions
+print("=======Accuracy Score===========")
+print(metrics.accuracy_score(y_test, y_pred_class))
+
+# print the confusion matrix
+print("=======Confision Matrix===========")
+metrics.confusion_matrix(y_test, y_pred_class)
+
+# print message text for false positives (ham incorrectly classifier)
+# X_test[(y_pred_class==1) & (y_test==0)]
+X_test[y_pred_class > y_test]
+
+# print message text for false negatives (spam incorrectly classifier)
+X_test[y_pred_class < y_test]
+
+# calculate predicted probabilities for X_test_dtm (poorly calibrated)
+y_pred_prob = nb.predict_proba(X_test_dtm)[:, 1]
+y_pred_prob
+
+# calculate AUC
+metrics.roc_auc_score(y_test, y_pred_prob)
+
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.pipeline import Pipeline
+
+pipe = Pipeline([('bow', CountVectorizer()), 
+                 ('tfid', TfidfTransformer()),  
+                 ('model', MultinomialNB())])
+
+pipe.fit(X_train, y_train)
+y_pred = pipe.predict(X_test)
+
+# calculate accuracy of class predictions
+print("=======Accuracy Score===========")
+print(metrics.accuracy_score(y_test, y_pred))
+
+# print the confusion matrix
+print("=======Confision Matrix===========")
+metrics.confusion_matrix(y_test, y_pred)
+
+# import an instantiate a logistic regression model
+from sklearn.linear_model import LogisticRegression
+
+logreg = LogisticRegression(solver='liblinear')
+
+# train the model using X_train_dtm
+%time logreg.fit(X_train_dtm, y_train)
+
+# make class predictions for X_test_dtm
+y_pred_class = logreg.predict(X_test_dtm)
+
+# calculate predicted probabilities for X_test_dtm (well calibrated)
+y_pred_prob = logreg.predict_proba(X_test_dtm)[:, 1]
+y_pred_prob
+
+# calculate accuracy of class predictions
+print("=======Accuracy Score===========")
+print(metrics.accuracy_score(y_test, y_pred_class))
+
+# print the confusion matrix
+print("=======Confision Matrix===========")
+print(metrics.confusion_matrix(y_test, y_pred_class))
+
+# calculate AUC
+print("=======ROC AUC Score===========")
+print(metrics.roc_auc_score(y_test, y_pred_prob))
+
+# remove English stop words
+vect = CountVectorizer(stop_words='english')
+
+# include 1-grams and 2-grams
+vect = CountVectorizer(ngram_range=(1, 2))
+
+# ignore terms that appear in more than 50% of the documents
+vect = CountVectorizer(max_df=0.5)
+
+# only keep terms that appear in at least 2 documents
+vect = CountVectorizer(min_df=2)
